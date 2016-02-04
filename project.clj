9692670c68cc06c497afddeede792a435b64230c
Daniel Anolik <dan@anolik.net>
2016-02-03 22:13:46 -0800

    switching from faraday to amazonica

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.taoensso/faraday "1.9.0-beta1"]
-                 [me.raynes/conch "0.8.0"]
+                 ;[com.taoensso/faraday "1.9.0-beta1"]
+                 ;[me.raynes/conch "0.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [uswitch/lambada "0.1.0"]]
+                 [uswitch/lambada "0.1.0"]
+                 [amazonica "0.3.49"]
+                 ]
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "bowlorama-history-tracker.jar")
